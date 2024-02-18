cbf26d26c64e49c6bf9ac421fb16ca3c30a3c402
Kevin Lowber <kjlow529@gmail.com>
2024-02-18 18:06:20 -0500

    Assignment 2

--- LSP_Project_Spring_2024/src/org/howard/edu/lsp/hw2/assignment2.java
+++ LSP_Project_Spring_2024/src/org/howard/edu/lsp/hw2/assignment2.java
@@ -7,17 +7,19 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
 
+//main function
 public class assignment2 {
     public static void main(String[] args) {
         if (args.length != 1) {
             System.err.println("Usage: java WordCounter <filename>");
             System.exit(1);
         }
-
+        //get filename
         String filename = args[0];
         numWords(filename);
     }
-
+    
+    //reading file, validating string requirements, adding to HashMap
     public static void numWords(String filename) {
         Map<String, Integer> wordCount = new HashMap<>();
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
@@ -31,20 +33,23 @@ public class assignment2 {
                     }
                 }
             }
+            //prints HashMap
             printWordCount(wordCount);
+          //error message 
         } catch (IOException e) {
             System.err.println("Error reading the file: " + e.getMessage());
         }
     }
-
+    
+    //method checking if word is valid (a string of all letters and longer than 3 letters)
     public static boolean isValidWord(String word) {
         return word.length()-1 > 3 && !isNumeric(word);
     }
-
+    //method that checks if the string has numbers
     public static boolean isNumeric(String str) {
         return Pattern.compile("[0-9]+").matcher(str).matches();
     }
-
+    //method that prints HashMap in line by line format (key, value)
     public static void printWordCount(Map<String, Integer> wordCount) {
         System.out.println("Word Count:");
         for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
